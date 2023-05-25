96178bb5611d905fed3ee3ff9e3d9795092633ac
YarianaU <yariana.ulatea@gmail.com>
2023-05-25 01:04:21 -0600

    ttop 10 y slider
    se agrega el top 10, el slider y se hacen pequeñas correcciones

--- casa_mia/casa_mia/index.php
+++ casa_mia/casa_mia/index.php
@@ -29,13 +29,13 @@
   <header>
     <div class="position-absolute top-0 start-0 pt-3 pb-3 ps-3 mobile-bar position-tab">
       <!-- <img class="mobile-icon" src="./icons/bars.svg" alt="menu bar"> -->
-      CASA MIA!
+      <span class="logo">CASA MIA!</span>
     </div>
     <!--  Menu navigation -->
     <nav id="navbar-main" class="navbar top-nav mobile-offcanvas navbar-expand-lg mx-auto color-light total-size">
       <div class="d-flex justify-content-between align-items-center ">
         <!-- <a href="index.html"><img src="./img/Logo.png" class="size position-relative ms-5 mobile-logo" alt="Graphic identifier"></a> -->
-        CASA MIA!
+        <span class="logo">CASA MIA!</span>
         <div class="offcanvas-header"><button id="btn-close" class="btn btn-light float-end me-5">X</button></div>
       </div>
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
@@ -53,15 +53,52 @@
   <!-- nav menu -->
 
   <!-- carrusel -->
-  <div>
-
+  <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
+    <div class="carousel-indicators">
+      <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+      <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+      <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+    </div>
+    <div class="carousel-inner">
+      <div class="carousel-item active">
+        <img src="./img/salmon.jpg" class="d-block w-100" alt="...">
+        <div class="carousel-caption d-none d-md-block">
+          <h5>First slide label</h5>
+          <p>Some representative placeholder content for the first slide.</p>
+        </div>
+      </div>
+      <div class="carousel-item">
+        <img src="./img/salmon.jpg" class="d-block w-100" alt="...">
+        <div class="carousel-caption d-none d-md-block">
+          <h5>Second slide label</h5>
+          <p>Some representative placeholder content for the second slide.</p>
+        </div>
+      </div>
+      <div class="carousel-item">
+        <img src="./img/salmon.jpg" class="d-block w-100" alt="...">
+        <div class="carousel-caption d-none d-md-block">
+          <h5>Third slide label</h5>
+          <p>Some representative placeholder content for the third slide.</p>
+        </div>
+      </div>
+    </div>
+    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Previous</span>
+    </button>
+    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Next</span>
+    </button>
   </div>
   <!-- carrusel -->
 
+  <hr class="mx-5 my-5">
+
   <!-- top recetas -->
   <section>
-    <div class="h4 pb-2 mb-4 color-green ">
-      <h2 class="title-margin ps-5 pe-5">TOP 10 RECETAS</h2>
+    <div class="h4 pb-2 mb-4 color-orange ">
+      <h2 class="title-margin ps-5 pe-5 text-shadow">TOP 10 RECETAS</h2>
     </div>
 
     <div class="card-container">
@@ -69,10 +106,171 @@
         <div class="image-container">
           <img src="./img/salmon.jpg" alt="Imagen 1" />
         </div>
-        <h4>Pasta en salas de queso</h4>
+        <h4 class="color-orange">Pasta en salas de queso</h4>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed pulvinar diam non risus porttitor. </p>
-        <span>Almuerzo</span>
-        <div class="d-flex justify-content-between"><button>Mas información</button><button>❤</button></div>
+        <span class="color-green stretched-link">Almuerzo</span>
+        <div class="d-flex justify-content-between btn-containers">
+          <button class="card-button">Mas información</button>
+          <button class="border-0" id="like">
+            <span>❤</span>
+          </button>
+          <button class="border-0 d-none" id="unlike">
+            <span class="text-danger">❤</span>
+          </button>
+        </div>
+      </div>
+      <div class="card">
+        <div class="image-container">
+          <img src="./img/salmon.jpg" alt="Imagen 1" />
+        </div>
+        <h4 class="color-orange">Pasta en salas de queso</h4>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed pulvinar diam non risus porttitor. </p>
+        <span class="color-green stretched-link">Almuerzo</span>
+        <div class="d-flex justify-content-between btn-containers">
+          <button class="card-button">Mas información</button>
+          <button class="border-0" id="like">
+            <span>❤</span>
+          </button>
+          <button class="border-0 d-none" id="unlike">
+            <span class="text-danger">❤</span>
+          </button>
+        </div>
+      </div>
+      <div class="card">
+        <div class="image-container">
+          <img src="./img/salmon.jpg" alt="Imagen 1" />
+        </div>
+        <h4 class="color-orange">Pasta en salas de queso</h4>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed pulvinar diam non risus porttitor. </p>
+        <span class="color-green stretched-link">Almuerzo</span>
+        <div class="d-flex justify-content-between btn-containers">
+          <button class="card-button">Mas información</button>
+          <button class="border-0" id="like">
+            <span>❤</span>
+          </button>
+          <button class="border-0 d-none" id="unlike">
+            <span class="text-danger">❤</span>
+          </button>
+        </div>
+      </div>
+      <div class="card">
+        <div class="image-container">
+          <img src="./img/salmon.jpg" alt="Imagen 1" />
+        </div>
+        <h4 class="color-orange">Pasta en salas de queso</h4>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed pulvinar diam non risus porttitor. </p>
+        <span class="color-green stretched-link">Almuerzo</span>
+        <div class="d-flex justify-content-between btn-containers">
+          <button class="card-button">Mas información</button>
+          <button class="border-0" id="like">
+            <span>❤</span>
+          </button>
+          <button class="border-0 d-none" id="unlike">
+            <span class="text-danger">❤</span>
+          </button>
+        </div>
+      </div>
+      <div class="card">
+        <div class="image-container">
+          <img src="./img/salmon.jpg" alt="Imagen 1" />
+        </div>
+        <h4 class="color-orange">Pasta en salas de queso</h4>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed pulvinar diam non risus porttitor. </p>
+        <span class="color-green stretched-link">Almuerzo</span>
+        <div class="d-flex justify-content-between btn-containers">
+          <button class="card-button">Mas información</button>
+          <button class="border-0" id="like">
+            <span>❤</span>
+          </button>
+          <button class="border-0 d-none" id="unlike">
+            <span class="text-danger">❤</span>
+          </button>
+        </div>
+      </div>
+      <div class="card">
+        <div class="image-container">
+          <img src="./img/salmon.jpg" alt="Imagen 1" />
+        </div>
+        <h4 class="color-orange">Pasta en salas de queso</h4>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed pulvinar diam non risus porttitor. </p>
+        <span class="color-green stretched-link">Almuerzo</span>
+        <div class="d-flex justify-content-between btn-containers">
+          <button class="card-button">Mas información</button>
+          <button class="border-0" id="like">
+            <span>❤</span>
+          </button>
+          <button class="border-0 d-none" id="unlike">
+            <span class="text-danger">❤</span>
+          </button>
+        </div>
+      </div>
+      <div class="card">
+        <div class="image-container">
+          <img src="./img/salmon.jpg" alt="Imagen 1" />
+        </div>
+        <h4 class="color-orange">Pasta en salas de queso</h4>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed pulvinar diam non risus porttitor. </p>
+        <span class="color-green stretched-link">Almuerzo</span>
+        <div class="d-flex justify-content-between btn-containers">
+          <button class="card-button">Mas información</button>
+          <button class="border-0" id="like">
+            <span>❤</span>
+          </button>
+          <button class="border-0 d-none" id="unlike">
+            <span class="text-danger">❤</span>
+          </button>
+        </div>
+      </div>
+      <div class="card">
+        <div class="image-container">
+          <img src="./img/salmon.jpg" alt="Imagen 1" />
+        </div>
+        <h4 class="color-orange">Pasta en salas de queso</h4>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed pulvinar diam non risus porttitor. </p>
+        <span class="color-green stretched-link">Almuerzo</span>
+        <div class="d-flex justify-content-between btn-containers">
+          <button class="card-button">Mas información</button>
+          <button class="border-0" id="like">
+            <span>❤</span>
+          </button>
+          <button class="border-0 d-none" id="unlike">
+            <span class="text-danger">❤</span>
+          </button>
+        </div>
+      </div>
+      <div class="card">
+        <div class="image-container">
+          <img src="./img/salmon.jpg" alt="Imagen 1" />
+        </div>
+        <h4 class="color-orange">Pasta en salas de queso</h4>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed pulvinar diam non risus porttitor. </p>
+        <span class="color-green stretched-link">Almuerzo</span>
+        <div class="d-flex justify-content-between btn-containers">
+          <button class="card-button">Mas información</button>
+          <button class="border-0" id="like">
+            <span>❤</span>
+          </button>
+          <button class="border-0 d-none" id="unlike">
+            <span class="text-danger">❤</span>
+          </button>
+        </div>
+      </div>
+      <div class="card">
+        <div class="image-container">
+          <img src="./img/salmon.jpg" alt="Imagen 1" />
+        </div>
+        <h4 class="color-orange">Pasta en salas de queso</h4>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed pulvinar diam non risus porttitor. </p>
+        <span class="color-green stretched-link">Almuerzo</span>
+        <div class="d-flex justify-content-between btn-containers">
+          <button class="card-button">Mas información</button>
+          <button class="border-0" id="like">
+            <span>❤</span>
+          </button>
+          <button class="border-0 d-none" id="unlike">
+            <span class="text-danger">❤</span>
+          </button>
+        </div>
       </div>
     </div>
 
@@ -106,14 +304,27 @@
   <!-- AOS Animation -->
   <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
   <script>
+    var likeButton = document.getElementById("like");
+    var unlikeButton = document.getElementById("unlike");
+
+    // Agregar controladores de eventos a los botones
+    likeButton.addEventListener("click", function() {
+      likeButton.classList.add("d-none");
+      unlikeButton.classList.remove("d-none");
+    });
+
+    unlikeButton.addEventListener("click", function() {
+      unlikeButton.classList.add("d-none");
+      likeButton.classList.remove("d-none");
+    });
+  </script>
+  <script>
     document.addEventListener("DOMContentLoaded", function() {
-
       AOS.init();
-
-      document.querySelector('.mobile-icon').addEventListener('click', function(event) {
-        console.log('click');
-        document.getElementById('navbar-main').classList.add('show-nav');
-      });
+      // document.querySelector('.mobile-icon').addEventListener('click', function(event) {
+      //   console.log('click');
+      //   document.getElementById('navbar-main').classList.add('show-nav');
+      // });
 
       document.querySelector('#btn-close').addEventListener('click', function(event) {
         document.getElementById('navbar-main').classList.remove('show-nav');
