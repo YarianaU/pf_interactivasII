323b797b3aec4ddd8ec5ad560a034ab61d2684ec
YarianaU <yariana.ulatea@gmail.com>
2023-05-27 02:09:00 -0600

    pagina de receta y botones de ingreso y registro
    Se agrega el modal de receta y los botones para ingreso y registro entre otras correcciones

--- casa_mia/casa_mia/index.php
+++ casa_mia/casa_mia/index.php
@@ -16,6 +16,8 @@
   <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
   <!-- boostrap -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
   <!-- JavaScript Bundle with Popper -->
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
   <link rel="stylesheet" href="https://unpkg.com/flickity@2/dist/flickity.min.css">
@@ -27,9 +29,13 @@
 <body>
   <!-- nav menu -->
   <header>
-    <div class="position-absolute top-0 start-0 pt-3 pb-3 ps-3 mobile-bar position-tab">
+    <div class="position-absolute top-0 start-0 pt-3 pb-3 ps-3 mobile-bar position-tab d-flex justify-content-between">
       <!-- <img class="mobile-icon" src="./icons/bars.svg" alt="menu bar"> -->
       <span class="logo">CASA MIA!</span>
+      <div>
+        <button class="header-button bg-orange">REGISTRO</button>
+        <button class="header-button bg-olive">INGRESO</button>
+      </div>
     </div>
     <!--  Menu navigation -->
     <nav id="navbar-main" class="navbar top-nav mobile-offcanvas navbar-expand-lg mx-auto color-light total-size">
@@ -42,10 +48,10 @@
         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
           <li class="top-nav-item"><a class="nav-link top-nav-link top-nav-item" aria-current="page" href="#">Contacto</a></li>
         </ul>
-        <!-- <form class="d-flex search-nav ps-5 pe-5 ">
-          <input class="form-control me-2 " type="text" placeholder="Buscar Recetas...">
-          <a href="#"><img src="icons/lupa.png" alt="Lupa"></a>
-        </form> -->
+        <div>
+          <button class="header-button bg-orange">REGISTRO</button>
+          <button class="header-button bg-olive">INGRESO</button>
+        </div>
       </div>
       <!--  responsive part -->
     </nav>
@@ -61,21 +67,21 @@
     </div>
     <div class="carousel-inner">
       <div class="carousel-item active">
-        <img src="./img/salmon.jpg" class="d-block w-100" alt="...">
+        <img src="./img/salmon.jpg" class="d-block w-100 h-100" alt="...">
         <div class="carousel-caption d-none d-md-block">
           <h5>First slide label</h5>
           <p>Some representative placeholder content for the first slide.</p>
         </div>
       </div>
       <div class="carousel-item">
-        <img src="./img/salmon.jpg" class="d-block w-100" alt="...">
+        <img src="./img/salmon.jpg" class="d-block w-100 h-100" alt="...">
         <div class="carousel-caption d-none d-md-block">
           <h5>Second slide label</h5>
           <p>Some representative placeholder content for the second slide.</p>
         </div>
       </div>
       <div class="carousel-item">
-        <img src="./img/salmon.jpg" class="d-block w-100" alt="...">
+        <img src="./img/salmon.jpg" class="d-block w-100 h-100" alt="...">
         <div class="carousel-caption d-none d-md-block">
           <h5>Third slide label</h5>
           <p>Some representative placeholder content for the third slide.</p>
@@ -106,7 +112,24 @@
         <div class="image-container">
           <img src="./img/salmon.jpg" alt="Imagen 1" />
         </div>
-        <h4 class="color-orange">Pasta en salas de queso</h4>
+        <h4 class="color-orange">Pasta en salsa de queso</h4>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed pulvinar diam non risus porttitor. </p>
+        <span class="color-green stretched-link">Almuerzo</span>
+        <div class="d-flex justify-content-between btn-containers">
+          <button class="card-button" data-bs-toggle="modal" data-bs-target="#recipeModal">Mas información</button>
+          <button class="border-0" id="like">
+            <span>❤</span>
+          </button>
+          <button class="border-0 d-none" id="unlike">
+            <span class="text-danger">❤</span>
+          </button>
+        </div>
+      </div>
+      <div class="card">
+        <div class="image-container">
+          <img src="./img/salmon.jpg" alt="Imagen 1" />
+        </div>
+        <h4 class="color-orange">Pasta en salsa de queso</h4>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed pulvinar diam non risus porttitor. </p>
         <span class="color-green stretched-link">Almuerzo</span>
         <div class="d-flex justify-content-between btn-containers">
@@ -123,7 +146,7 @@
         <div class="image-container">
           <img src="./img/salmon.jpg" alt="Imagen 1" />
         </div>
-        <h4 class="color-orange">Pasta en salas de queso</h4>
+        <h4 class="color-orange">Pasta en salsa de queso</h4>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed pulvinar diam non risus porttitor. </p>
         <span class="color-green stretched-link">Almuerzo</span>
         <div class="d-flex justify-content-between btn-containers">
@@ -140,7 +163,7 @@
         <div class="image-container">
           <img src="./img/salmon.jpg" alt="Imagen 1" />
         </div>
-        <h4 class="color-orange">Pasta en salas de queso</h4>
+        <h4 class="color-orange">Pasta en salsa de queso</h4>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed pulvinar diam non risus porttitor. </p>
         <span class="color-green stretched-link">Almuerzo</span>
         <div class="d-flex justify-content-between btn-containers">
@@ -157,7 +180,7 @@
         <div class="image-container">
           <img src="./img/salmon.jpg" alt="Imagen 1" />
         </div>
-        <h4 class="color-orange">Pasta en salas de queso</h4>
+        <h4 class="color-orange">Pasta en salsa de queso</h4>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed pulvinar diam non risus porttitor. </p>
         <span class="color-green stretched-link">Almuerzo</span>
         <div class="d-flex justify-content-between btn-containers">
@@ -174,7 +197,7 @@
         <div class="image-container">
           <img src="./img/salmon.jpg" alt="Imagen 1" />
         </div>
-        <h4 class="color-orange">Pasta en salas de queso</h4>
+        <h4 class="color-orange">Pasta en salsa de queso</h4>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed pulvinar diam non risus porttitor. </p>
         <span class="color-green stretched-link">Almuerzo</span>
         <div class="d-flex justify-content-between btn-containers">
@@ -191,7 +214,7 @@
         <div class="image-container">
           <img src="./img/salmon.jpg" alt="Imagen 1" />
         </div>
-        <h4 class="color-orange">Pasta en salas de queso</h4>
+        <h4 class="color-orange">Pasta en salsa de queso</h4>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed pulvinar diam non risus porttitor. </p>
         <span class="color-green stretched-link">Almuerzo</span>
         <div class="d-flex justify-content-between btn-containers">
@@ -208,7 +231,7 @@
         <div class="image-container">
           <img src="./img/salmon.jpg" alt="Imagen 1" />
         </div>
-        <h4 class="color-orange">Pasta en salas de queso</h4>
+        <h4 class="color-orange">Pasta en salsa de queso</h4>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed pulvinar diam non risus porttitor. </p>
         <span class="color-green stretched-link">Almuerzo</span>
         <div class="d-flex justify-content-between btn-containers">
@@ -225,7 +248,7 @@
         <div class="image-container">
           <img src="./img/salmon.jpg" alt="Imagen 1" />
         </div>
-        <h4 class="color-orange">Pasta en salas de queso</h4>
+        <h4 class="color-orange">Pasta en salsa de queso</h4>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed pulvinar diam non risus porttitor. </p>
         <span class="color-green stretched-link">Almuerzo</span>
         <div class="d-flex justify-content-between btn-containers">
@@ -242,24 +265,7 @@
         <div class="image-container">
           <img src="./img/salmon.jpg" alt="Imagen 1" />
         </div>
-        <h4 class="color-orange">Pasta en salas de queso</h4>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed pulvinar diam non risus porttitor. </p>
-        <span class="color-green stretched-link">Almuerzo</span>
-        <div class="d-flex justify-content-between btn-containers">
-          <button class="card-button">Mas información</button>
-          <button class="border-0" id="like">
-            <span>❤</span>
-          </button>
-          <button class="border-0 d-none" id="unlike">
-            <span class="text-danger">❤</span>
-          </button>
-        </div>
-      </div>
-      <div class="card">
-        <div class="image-container">
-          <img src="./img/salmon.jpg" alt="Imagen 1" />
-        </div>
-        <h4 class="color-orange">Pasta en salas de queso</h4>
+        <h4 class="color-orange">Pasta en salsa de queso</h4>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed pulvinar diam non risus porttitor. </p>
         <span class="color-green stretched-link">Almuerzo</span>
         <div class="d-flex justify-content-between btn-containers">
@@ -281,13 +287,11 @@
   <footer class="top-nav container-fluid footer">
     <div class="row footer-contacts ">
 
-      <h4 class=" contacts-title color-w title-margin">CONTACTOS</h4>
-
-      <ul class="text-center center-information d-flex justify-content-between w-50 m-auto">
-        <li class="invisible-point mb-3 "><a class="footer-text contact-text color-w fs-5" href="#">sobre nosotros</a></li>
-        <li class="invisible-point mb-3 "><a class="footer-text contact-text color-w fs-5" href="#">sobre nosotros</a></li>
-        <li class="invisible-point mb-3 "><a class="footer-text contact-text color-w fs-5" href="#">sobre nosotros</a></li>
-        <li class="invisible-point mb-3 "><a class="footer-text contact-text color-w fs-5" href="#">sobre nosotros</a></li>
+      <ul class="text-center center-information d-flex justify-content-between w-50 m-auto mt-5">
+        <li class="invisible-point mb-3 "><a class="footer-text contact-text color-w fs-5" href="#">Sobre nosotros</a></li>
+        <li class="invisible-point mb-3 "><a class="footer-text contact-text color-w fs-5" href="#">Ayuda</a></li>
+        <li class="invisible-point mb-3 "><a class="footer-text contact-text color-w fs-5" href="#">Desarrolladores</a></li>
+        <li class="invisible-point mb-3 "><a class="footer-text contact-text color-w fs-5" href="#">Terminos y condiciones</a></li>
       </ul>
 
       <div class="legal-text social-media ms-1">
@@ -295,11 +299,44 @@
         <a href="#" class="social-media-link"><img class="icon-size red-margin social-media-img" src="./icons/instagram.png" alt="instagram"></a>
         <a href="#" class="social-media-link"><img class="icon-size red-margin social-media-img" src="./icons/youtube.png" alt="youtube"></a>
         <div class="line btn-margin"></div>
-        <h6 class="text-center color-w margin-text"> ©2022 Foodies. Todos los derechos reservados.</h6>
+        <h6 class="text-center color-w margin-text"> ©2022 Casa Mia. Todos los derechos reservados.</h6>
       </div>
     </div>
   </footer>
   <!-- Footer -->
+
+  <!-- Modal recetas -->
+  <div class="modal fade" id="recipeModal" tabindex="-1" aria-labelledby="recipeModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered modal-xl">
+      <div class="modal-content">
+        <div class="modal-body">
+          <button class="color-orange close-button" data-bs-dismiss="modal" aria-label="Close">X</button>
+          <div class="d-flex">
+            <div>
+              <img class="recipe-img" src="./img/salmon.jpg" alt="">
+            </div>
+            <div class="recipe-data">
+              <span>Preparación: <span>25 min</span></span>
+              <span>Cocción: <span>10 min</span></span>
+              <span>Tiempo Total: <span>35 min</span></span>
+              <span>Porciones: <span>3 porciones</span></span>
+              <span>Dificultad: <span>Facíl</span></span>
+              <span>Destacada: <span>Si</span></span>
+              <hr class="w-100">
+              <div class="d-flex justify-content-between">
+                <span class="fs-5 color-green">Almuerzo</span>
+                <span class="fs-5 color-green">Verano</span>
+              </div>
+            </div>
+          </div>
+          <div>
+            <h2>Pasta en salsa de queso</h2>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- Modal recetas -->
 
   <!-- AOS Animation -->
   <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
